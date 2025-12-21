d4e1c1862b2691d142665420141f91a39c6b7d05
ashwin91108@gmail.com <ashwin.chandra08@gmail.com>
2025-12-21 23:40:53 +0530

    update main.jsx to prevent double api calls in prod

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const isDev = import.meta.env.MODE === 'development';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  isDev ? (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ) : (
     <App />
-  </React.StrictMode>,
+  )
 )
